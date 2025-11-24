21d3251bc4787daf87698d139a8ad68cc110193c
kostantis <smavraganiskostantis@gmail.com>
2025-11-24 13:18:08 +0200

    Initial commit

---
+++ src/test/java/gr/hua/dit/fittrack/FitTrackApplicationTests.java
@@ -0,0 +1,13 @@
+package gr.hua.dit.fittrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitTrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
