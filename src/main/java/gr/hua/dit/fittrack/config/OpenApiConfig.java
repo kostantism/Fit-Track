bac11568adf1c95d20af464275e090a5db28f3ea
kostantis <smavraganiskostantis@gmail.com>
2025-12-19 01:50:12 +0200

    swagger

--- src/main/java/gr/hua/dit/fittrack/config/OpenApiConfig.java
+++ src/main/java/gr/hua/dit/fittrack/config/OpenApiConfig.java
@@ -1,4 +1,47 @@
 package gr.hua.dit.fittrack.config;
 
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+
+import io.swagger.v3.oas.models.info.Info;
+
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * OpenAPI (Swagger) configuration.
+ */
+@Configuration
 public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Fit Track API")
+                        .version("v1")
+                        .description("Stateless API for the FitTrack application")
+                )
+                .components(new Components()
+                        .addSecuritySchemes("BearerAuth", new SecurityScheme()
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")
+                        )
+                )
+                .addSecurityItem(new SecurityRequirement().addList("BearerAuth"));
+    }
+
+    @Bean
+    public GroupedOpenApi groupedOpenApi() {
+        return GroupedOpenApi.builder()
+                .group("api")
+                .packagesToScan("gr.hua.dit.fittrack.web.rest")
+                .pathsToMatch("/api/v1/**")
+                .build();
+    }
 }
