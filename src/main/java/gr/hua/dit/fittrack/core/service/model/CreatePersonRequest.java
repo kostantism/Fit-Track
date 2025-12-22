47c2b966b67258430e835cd86adfb79d731a19f3
PanagiotisDimopoulos <it2023096@hua.gr>
2025-12-22 17:24:53 +0200

    RegistrationController

--- src/main/java/gr/hua/dit/fittrack/core/service/model/CreatePersonRequest.java
+++ src/main/java/gr/hua/dit/fittrack/core/service/model/CreatePersonRequest.java
@@ -1,20 +1,16 @@
 package gr.hua.dit.fittrack.core.service.model;
 
 import gr.hua.dit.fittrack.core.model.PersonType;
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
 
 /**
  * DTO for requesting the creation (registration) of a Person.
  */
 public record CreatePersonRequest(
-        @NotNull PersonType type,
-        @NotNull @NotBlank @Size(max = 100) String firstName,
-        @NotNull @NotBlank @Size(max = 100) String lastName,
-        @NotNull @NotBlank @Size(max = 100) @Email String emailAddress,
-        @NotNull @NotBlank @Size(max = 18) String mobilePhoneNumber,
-        @NotNull @NotBlank @Size(min = 4, max = 24) String rawPassword
+        PersonType type,
+        String firstName,
+        String lastName,
+        String emailAddress,
+        String mobilePhoneNumber,
+        String rawPassword
 ) {
 }
