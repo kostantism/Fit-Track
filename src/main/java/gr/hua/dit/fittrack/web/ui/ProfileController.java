fb04767174fe7642fe9f6b3a39ff4ba393827041
PanagiotisDimopoulos <it2023096@hua.gr>
2025-12-31 14:22:23 +0200

    Profile ProfileController

--- src/main/java/gr/hua/dit/fittrack/web/ui/ProfileController.java
+++ src/main/java/gr/hua/dit/fittrack/web/ui/ProfileController.java
@@ -4,7 +4,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 /**
- * UI controller for managing profile.
+ * UI controller for profile page.
+ *
+ * The authenticated user is provided automatically
+ * via CurrentUserControllerAdvice as model attribute "me".
  */
 @Controller
 public class ProfileController {
@@ -14,3 +17,22 @@ public class ProfileController {
         return "profile";
     }
 }
+
+
+
+//package gr.hua.dit.fittrack.web.ui;
+//
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.GetMapping;
+//
+///**
+// * UI controller for managing profile.
+// */
+//@Controller
+//public class ProfileController {
+//
+//    @GetMapping("/profile")
+//    public String showProfile() {
+//        return "profile";
+//    }
+//}
